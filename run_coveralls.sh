613ad8a8a92e12bb20b9319f91e8426125c6bae6
Pål-Erik Martinsen <palerikm@gmail.com>
2015-02-18 14:01:13 +0100

    More coveralls testing.

---
+++ run_coveralls.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+if [ "$CC" = "gcc" ]; then
+    echo expression evaluated as true
+    ./configure --with-check --enable-gcov && make && make check
+    coveralls --exclude-pattern ".+?\.h" --exclude-pattern "\w+/test" --gcov-options '\-lp'
+fi
