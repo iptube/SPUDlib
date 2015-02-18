85de614fa16d3b619d508deb5b8cee28122f0a75
Pål-Erik Martinsen <palerikm@gmail.com>
2015-02-18 15:40:20 +0100

    This is annoying

--- run_coveralls.sh
+++ run_coveralls.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-echo $COVERAGE
 
-if [ "$COVERAGE" = "true" ]; then
-    echo expression evaluated as true
-    ./configure --with-check --enable-gcov && make && make check
-    coveralls --exclude-pattern ".+?\.h" --exclude-pattern "\w+/test" --gcov-options '\-lp'
-fi
+./configure --with-check --enable-gcov && make && make check
+coveralls --exclude-pattern ".+?\.h" --exclude-pattern "\w+/test" --gcov-options '\-lp'
+
