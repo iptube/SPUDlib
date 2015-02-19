5e56f28c4c7d01a602a23450f5c78e725e7cc2eb
Joe Hildebrand <joe-github@cursive.net>
2015-02-19 14:39:31 -0700

    Move randomness to arc4 or urandom, if possible, and error if not (e.g. Windows)

--- src/spudlib.c
+++ src/spudlib.c
@@ -1,8 +1,10 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
 
 #include "spudlib.h"
+#include "../config.h"
 
 bool spud_isSpud(const uint8_t *payload, uint16_t length)
 {
@@ -11,21 +13,28 @@ bool spud_isSpud(const uint8_t *payload, uint16_t length)
 
 bool spud_createId(struct SpudMsgId *id)
 {
-    FILE *rfile = fopen("/dev/random", "r");
-    size_t i;
-
+#ifdef HAVE_ARC4RANDOM
+    arc4random_buf(id, sizeof(*id));
+    return true;
+#elif defined(HAVE__DEV_URANDOM)
+    // TODO: think about pre-reading entropy to avoid blocking I/O here.
+    // *certainly* don't open/close the file every time.
+    // Also, read these:
+    // http://insanecoding.blogspot.com/2014/05/a-good-idea-with-bad-usage-devurandom.html
+    // http://www.2uo.de/myths-about-urandom/
+    FILE *rfile = fopen("/dev/urandom", "r");
     if (!rfile) {
-        perror("Error opening /dev/random");
+        perror("Error opening /dev/urandom");
         //Do something else?
         return false;
     }
-    
-    for (i=0; i<sizeof(*id); i++) {
-        id->octet[i] |= fgetc(rfile);
-    }
-    
+
+    size_t nread = fread(id, sizeof(*id), 1, rfile);
     fclose(rfile);
-    return true;
+    return nread == sizeof(*id);
+#else
+  #error New random source needed
+#endif
 }
 
 
@@ -35,7 +44,7 @@ bool spud_setId(struct SpudMsg *msg,const struct SpudMsgId *id)
         return false;
     }
     memcpy(&msg->msgHdr.id, id, SPUD_MSG_ID_SIZE);
-    
+
     return true;
 }
 
@@ -51,7 +60,7 @@ char* spud_idToString(char* buf, size_t len, const struct SpudMsgId *id)
     if(len < SPUD_MSG_ID_SIZE*2+1){
         return NULL;
     }
-    
+
     for(i=0;i<SPUD_MSG_ID_SIZE;i++){
         sprintf(buf+2*i,"%02x",id->octet[i]);
     }
