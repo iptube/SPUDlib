dd40c052b9761a0666d1a7cce35cefd872950d6e
Pål-Erik Martinsen <palerikm@gmail.com>
2015-03-04 05:22:10 -0800

    Travis did not like the --no-external argument to lcov.

--- coverage.sh
+++ coverage.sh
@@ -3,6 +3,6 @@
 make clean
 make check
 
-lcov -d . --no-external --capture --directory src --output-file spudlib.info
+lcov -d . --capture --directory src --output-file spudlib.info
 coveralls-lcov spudlib.info
 
