6088cbbe080c12cdb0da1f60bf7a32ee9c28805e
Pål-Erik Martinsen <palerikm@gmail.com>
2015-03-05 10:07:36 +0100

    Fixing gcov reports using lcov on osx.

--- coveragereport.sh
+++ coveragereport.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-./coverage.sh
-lcov --directory src --capture --output-file spudlib.info
+#./configure --with-check --enable-gcov --disable-shared
+./configure --with-check --enable-gcov
+make clean
+make check
+
+lcov --directory src --capture --no-external --output-file spudlib.info
 genhtml --output-directory lcov spudlib.info
 echo "Open lcov/index.html with your favourite browser"
