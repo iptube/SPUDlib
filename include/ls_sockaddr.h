29ad635fdbcacc779b0e2fa1289919d4f077e4e3
Pål-Erik Martinsen <palerikm@gmail.com>
2015-03-10 14:37:30 +0100

    Added tube tests, and moved added ip helper function to ls_sockaddr

--- include/ls_sockaddr.h
+++ include/ls_sockaddr.h
@@ -4,5 +4,11 @@
 #include <netinet/in.h>
 
 #include "ls_basics.h"
+#include "ls_error.h"
 
 LS_API size_t ls_sockaddr_get_length(const struct sockaddr *addr);
+
+LS_API bool ls_get_remote_ip_addr(struct sockaddr_in6 *remoteAddr,
+                                  const char *fqdn,
+                                  const char *port,
+                                  ls_err *err);
