8c130fa14be0afd38d8cfff5c625578c3cecb41f
Joe Hildebrand <joe-github@cursive.net>
2015-03-28 19:32:33 -0600

    move coverage reporting into (c)make

--- coveragereport.sh
+++ coveragereport.sh
@@ -2,5 +2,4 @@
 if [ ! -d "build" ]; then
     mkdir build
 fi
-cd build && cmake .. -DCMAKE_BUILD_TYPE=Debug -Dcoveralls=ON -Dcoveralls_send=OFF && make all coveralls && lcov --directory src/CMakeFiles/spud.dir --capture --output-file spudlib.info &&  genhtml --output-directory lcov spudlib.info
-echo "Open build/lcov/index.html with your favourite browser"
+cd build && cmake .. -DCMAKE_BUILD_TYPE=Debug -Dcoveralls=ON -Dcoveralls_send=OFF && make all coveralls coverage_report
