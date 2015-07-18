34e66f00fd02abcb43aedff63875f4b196752ca1
Joe Hildebrand <joe-github@cursive.net>
2015-07-18 17:17:18 +0200

    actually *call* the ls_timer_is_cancelled function

--- src/ls_timer.c
+++ src/ls_timer.c
@@ -83,7 +83,7 @@ LS_API void * ls_timer_get_context(ls_timer *tim)
 LS_API struct timeval * ls_timer_get_time(ls_timer *tim)
 {
     assert(tim);
-    if (ls_timer_is_cancelled) {
+    if (ls_timer_is_cancelled(tim)) {
         return NULL;
     }
     return &tim->tv;
